553b83cae0eab488ff033dceedbd9f179900572f
soyun2 <tkdtod0619@naver.com>
2021-07-01 15:43:27 +0900

    .

--- ATM/ATM/Panel/UCP_Login.cs
+++ ATM/ATM/Panel/UCP_Login.cs
@@ -32,70 +32,77 @@ namespace ATM.Panel
 
         private void button_login_Click(object sender, EventArgs e)
         {
-            string connStr = "Server=192.168.0.104;Database=atm;Uid=root;Pwd=1234;";
-            int result = 0;
-
-            using (MySqlConnection conn = new MySqlConnection(connStr))
+            try
             {
-                conn.Open();
-                string sql = "SELECT count(*) as cnt, bank, name, balance  FROM account where acc_num = '" + textBox_account.Text + "' and pwd = '" + textBox_pwd.Text + "';";
-                //ExecuteReader를 이용하여
-                //연결 모드로 데이타 가져오기
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
+                string connStr = "Server=192.168.0.104;Database=atm;Uid=root;Pwd=1234;";
+                int result = 0;
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
                 {
-                    result=Convert.ToInt32(rdr["cnt"]);
-                    bank = rdr["bank"].ToString();
-                    name = rdr["name"].ToString();
-                    balance = Convert.ToInt32(rdr["balance"]);
-                    Console.WriteLine("{0}", result);
+                    conn.Open();
+                    string sql = "SELECT count(*) as cnt, bank, name, balance  FROM account where acc_num = '" + textBox_account.Text + "' and pwd = '" + textBox_pwd.Text + "';";
+                    //ExecuteReader를 이용하여
+                    //연결 모드로 데이타 가져오기
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        result = Convert.ToInt32(rdr["cnt"]);
+                        bank = rdr["bank"].ToString();
+                        name = rdr["name"].ToString();
+                        balance = Convert.ToInt32(rdr["balance"]);
+                        Console.WriteLine("{0}", result);
+                    }
+                    rdr.Close();
                 }
-                rdr.Close();
-            }
-            //박상준,20210629
-            //일지하는 정보(result)가 있으면 원하는 패널로 이동
-            if(result==0)
-            {
-                textBox_account = null;
-                textBox_pwd = null;
-                MessageBox.Show("일치하는 정보가 없습니다");
-            }
-            else if(result ==1)
-            {
-                //form에 로그인한 정보 저장
-                parentForm.login(textBox_account.Text,bank,name,balance);
-
-                switch (panelNum)
+                //박상준,20210629
+                //일지하는 정보(result)가 있으면 원하는 패널로 이동
+                if (result == 0)
                 {
-                    case 0://환전
-                        UCP_Exchange exchange = new UCP_Exchange(parentForm);
-                        parentForm.controllView(exchange);
-                        break;
-                    case 1: //송금
-                        UCP_Transaction trans = new UCP_Transaction(parentForm);
-                        parentForm.controllView(trans);
-                        break;
-                    case 2: //업비트 (코인 거래 화면으로)
-                        UCP_CList clist = new UCP_CList(parentForm);
-                        parentForm.controllView(clist);
-                        break;
-                    case 3: // 코인 보유 현황 조회
-                        UCP_MyCoin mycoin = new UCP_MyCoin(parentForm);
-                        parentForm.controllView(mycoin);
-                        break;
-                    case 4: // 거래내역 조회
-                        Panel.UCP_Transaction_history panel10 = new Panel.UCP_Transaction_history(parentForm);
-                        parentForm.controllView(panel10);
-                        break;
-                    case 5: // 홈화면으로
-                        parentForm.HomePanel();
-                        break;
-                    default:
-                       
-                        break;
-                }              
+                    textBox_account = null;
+                    textBox_pwd = null;
+                    MessageBox.Show("일치하는 정보가 없습니다");
+                }
+                else if (result == 1)
+                {
+                    //form에 로그인한 정보 저장
+                    parentForm.login(textBox_account.Text, bank, name, balance);
+
+                    switch (panelNum)
+                    {
+                        case 0://환전
+                            UCP_Exchange exchange = new UCP_Exchange(parentForm);
+                            parentForm.controllView(exchange);
+                            break;
+                        case 1: //송금
+                            UCP_Transaction trans = new UCP_Transaction(parentForm);
+                            parentForm.controllView(trans);
+                            break;
+                        case 2: //업비트 (코인 거래 화면으로)
+                            UCP_CList clist = new UCP_CList(parentForm);
+                            parentForm.controllView(clist);
+                            break;
+                        case 3: // 코인 보유 현황 조회
+                            UCP_MyCoin mycoin = new UCP_MyCoin(parentForm);
+                            parentForm.controllView(mycoin);
+                            break;
+                        case 4: // 거래내역 조회
+                            Panel.UCP_Transaction_history panel10 = new Panel.UCP_Transaction_history(parentForm);
+                            parentForm.controllView(panel10);
+                            break;
+                        case 5: // 홈화면으로
+                            parentForm.HomePanel();
+                            break;
+                        default:
+
+                            break;
+                    }
+                }
             }
+            catch (InvalidCastException)
+            {
+                MessageBox.Show("계좌번호와 비밀번호를 입력하세요.");
+            }            
         }
     }
 }
